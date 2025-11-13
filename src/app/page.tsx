8b94c83a4e571fc4f69b449277cae62fb8117f06
Dee Caulcrick <deecaulcrick@gmail.com>
2025-11-13 12:01:28 +0100

    Adjusted bottom padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-linear-to-b from-[#96BBCD] to-[#FFFFFF] py-12  flex items-center justify-center">
+    <div className="bg-linear-to-b from-[#96BBCD] to-[#FFFFFF] pt-12  flex items-center justify-center">
       <div className="container p-8 md:p-12 flex flex-col min-h-screen">
         <Hero />
         <div className="mt-12 grow mb-10">
